25f6ec3bfd6f8c8f20544b12e0ebf1f55a4a2a07
luisalbertoruiz <luisalbertoruiz.geek@gmail.com>
2015-07-14 11:52:40 -0500

    database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'pumasruiz',
 			'username'  => 'starkiller',
-			'password'  => '1234',
+			'password'  => '5639119833Mx',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
