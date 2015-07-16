f87e671ef665da01b70423ef0c8a1c3b911f3074
kobe <luisalbertoruiz.geek@gmail.com>
2015-07-16 18:36:18 -0500

    se agrega meta para bing

--- app/views/home/footer.blade.php
+++ app/views/home/footer.blade.php
@@ -2,7 +2,7 @@
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 		<footer class="panel panel-default" style="margin-bottom:0 !important;">
 		  <div class="panel-body">
-				
+				{{ HTML::image('src/logo-blanco-sm.png', 'pumasruiz',array('class'=>'home pull-right')) }}
 		  </div>
 		</footer>
 	</div>	
--- app/views/layout/metas.blade.php
+++ app/views/layout/metas.blade.php
@@ -1,2 +1,3 @@
-<meta name="author" content="xolotsoft">
-<meta name="Description" content="Club de Futbol Amateur Pumas Ruiz">
+<meta name="author" content="xolotsoft" />
+<meta name="description" content="Club de Futbol Amateur Pumas Ruiz" />
+<meta name="msvalidate.01" content="CA549D5B7D7D2E6652E6E1BD4516BB00" />
