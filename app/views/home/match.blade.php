9bbd358ce885236426092b794ad5509bbe10a8c1
luisalbertoruiz <luisalbertoruiz.geek@gmail.com>
2015-07-20 11:13:24 -0500

    marcador estatico del dia 19 de julio

--- app/views/home/match.blade.php
+++ app/views/home/match.blade.php
@@ -3,8 +3,8 @@
 	   <div id="ultimo" class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 	   		<h4>Último partido</h4>
 	   		{{ HTML::image('src/escudos/pumas.png', 'local',array('id'=>'local','title'=>'Pumas Ruiz FC')) }}
-	   		{{ HTML::image('src/escudos/cfire.png', 'visita',array('id'=>'visita','title'=>'Chicago Fire')) }}
-	   		<h2>1 - 3</h2>
+	   		{{ HTML::image('src/escudos/cristobal.png', 'visita',array('id'=>'visita','title'=>'San Cristobal')) }}
+	   		<h2>1 - 2</h2>
 	   </div>
 	   <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="proximo">
 	   		<h4>Próximo partido</h4>
