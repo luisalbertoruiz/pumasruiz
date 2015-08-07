be0b323e48619a8fddef7ad1bbfb7a051ce0264d
kobe <luisalbertoruiz.geek@gmail.com>
2015-08-06 21:47:42 -0500

    actualizacion estatica fecha 5 river

--- app/views/home/match.blade.php
+++ app/views/home/match.blade.php
@@ -9,7 +9,7 @@
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="proximo">
             <h4>Próximo partido</h4>
             {{ HTML::image('src/escudos/pumas.png', 'local',array('id'=>'local','title'=>'Pumas Ruiz FC')) }}
-            {{ HTML::image('src/escudos/default.png', 'visita',array('id'=>'visita','title'=>'local')) }}
+            {{ HTML::image('src/escudos/river.png', 'visita',array('id'=>'visita','title'=>'River Plate')) }}
             <h3>vs</h3>
             <h3>sabado 5, 17:00 hrs</h3>
             <h3>Vicente Guerrero</h3>
