2b82ef959937668518d1086406349c568bf6c751
kobe <luisalbertoruiz.geek@gmail.com>
2015-08-09 11:51:31 -0500

    miniaturas src

--- app/views/home/notice.blade.php
+++ app/views/home/notice.blade.php
@@ -9,28 +9,28 @@
                     <li>
                         <h4><a href="" class="linkNoticia">Primer Derrota</a></h4>
                         <h6>12 Julio 2015</h6>
-                        <a href="">	{{ HTML::image('src/galeria/2s.jpg', 'pumasruiz',array('class'=>'notImg')) }}</a>
+                        <a href="">	{{ HTML::image('src/miniaturas/1s-min.jpg', 'pumasruiz',array('class'=>'notImg')) }}</a>
                         <p class="noticiaP">Segundo Partido y ya tuvimos la primer derrota, despues de ir perdiendo dos goles a cero, en un tiro de Castigo "Ale" Anota un Hermoso Gol, el tiro sale con bastante chanfle muy duro, pasa arriba de la barrera y se incrusta en el mero angulo, se sigue peleando duro por los dos equipos, debemos confesar que casi siempre el Chicago Fire nos Gano la Media, y a sufrir se ha dicho perdiendiendonos la oportunidad de empatar el partido.</p>
                         <a href="" class="seguirNoticia">Seguir leyendo</a>
                     </li><hr>
                     <li>
                         <h4><a href="" class="linkNoticia">¡Penalti!</a></h4>
                         <h6>12 Julio 2015</h6>
-                        {{ HTML::image('src/galeria/1s.jpg', 'pumasruiz',array('class'=>'notImg')) }}
+                        {{ HTML::image('src/miniaturas/2s-min.jpg', 'pumasruiz',array('class'=>'notImg')) }}
                         <p class="noticiaP">Faltando 10 minutos para concluir el partido, "Hugo" Pelea un balon y lo controla en el area Grande lo cubre perfectamente y llega el defensa y le comete falta, y el arbitro dictamina el penalti, responsabilidad que se le asigna a "Ale" quien se perfila a tirarlo y en esta ocasión se perfrila mal y le pega horrible al balon mandandolo muy lejos de la porteria.</p>
                         <a href="" class="seguirNoticia">Seguir leyendo</a>
                     </li><hr>
                         <li>
                         <h4>Falta de Contundencia</h4>
                         <h6>12 Julio 2015</h6>
-                        {{ HTML::image('src/galeria/1s.jpg', 'pumasruiz',array('class'=>'notImg')) }}
+                        {{ HTML::image('src/miniaturas/3s-min.jpg', 'pumasruiz',array('class'=>'notImg')) }}
                         <p class="noticiaP">Se efectua una descolgada del equipo contrario y nos anota el gol, dandose el viejo dicho de que el que perdona pierde. tambien cabe aclara que el "Chime" Tuvo cuando menos 2 oportunidades de anotar goles muy claramente en el 1er tiempo y perdono, asi que, pues se lucho, pero la falta de contundencia nos hizo perder el partido.</p>
                         <a href="" class="seguirNoticia">Seguir leyendo</a>
                     </li><hr>
                         <li>
                         <h4>Que par de osos</h4>
                         <h6>12 Julio 2015</h6>
-                        {{ HTML::image('src/galeria/3s.jpg', 'pumasruiz',array('class'=>'notImg')) }}
+                        {{ HTML::image('src/miniaturas/4s-min.jpg', 'pumasruiz',array('class'=>'notImg')) }}
                         <p class="noticiaP">Se cometio dos errores fundamnetales en la porteria, en el 1er Gol "Irwin" ataca el tiro con mala tecnica y le dobla las manos entrando el Gol, y en el 2do Gol "Moi" un portero con mas experiencia, no la aplica y le rematan enfrente de el en el area chica con un cabezazo que bien pudo evitarlo pues es su area en fin todo se complico, aunque la jugada clave fue la de el penalti, estos errores se van a ir puliendo conforme vaya jugando los jugadores mas acoplados como equipo.</p>
                         <a href="" class="seguirNoticia">Seguir leyendo</a>
                     </li>
