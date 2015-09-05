b79d67bce96811c9c78ae7aa3ef8e4b33d79093f
kobe <luisalbertoruiz.geek@gmail.com>
2015-09-05 12:45:10 -0500

    fecha 10 estatica

--- app/views/home/match.blade.php
+++ app/views/home/match.blade.php
@@ -3,16 +3,16 @@
         <div id="ultimo" class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
             <h4>Último partido</h4>
             {{ HTML::image('src/escudos/pumas.png', 'local',array('id'=>'local','title'=>'Pumas Ruiz FC')) }}
-            {{ HTML::image('src/escudos/cruz_azul.png', 'visita',array('id'=>'visita','title'=>'Maquina')) }}
-            <h2>3 - 1</h2>
+            {{ HTML::image('src/escudos/Everton.png', 'visita',array('id'=>'visita','title'=>'Everton')) }}
+            <h2>2 - 3</h2>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="proximo">
             <h4>Próximo partido</h4>
             {{ HTML::image('src/escudos/pumas.png', 'local',array('id'=>'local','title'=>'Pumas Ruiz FC')) }}
-            {{ HTML::image('src/escudos/Everton.png', 'visita',array('id'=>'visita','title'=>'Everton')) }}
+            {{ HTML::image('src/escudos/default.png', 'visita',array('id'=>'visita','title'=>'Juventud')) }}
             <h3>vs</h3>
-            <h3>domingo 30, 9:30 hrs</h3>
-            <h3>Vicente Guerrero</h3>
+            <h3>domingo 6, 9:30 hrs</h3>
+            <h3>Morelos II</h3>
         </div>
     </div>
 </div>
