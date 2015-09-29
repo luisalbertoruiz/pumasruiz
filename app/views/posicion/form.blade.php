f4e01ac9aa146938c43377656d3ae9ee07fb98ce
luisalbertoruiz <luisalbertoruiz.geek@gmail.com>
2015-09-28 20:51:23 -0500

    form posiciones

--- app/views/posicion/form.blade.php
+++ app/views/posicion/form.blade.php
@@ -1,7 +1,7 @@
 <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 	<div class="form-group">
 		{{ Form::label('equipo_id' , 'Equipo')}}
-		{{ Form::select('equipo_id', $equipos, null, array('class'=>'form-control','required'=>'true'))}}<br>
+		{{ Form::select('equipo_id', array('' => '') +$equipos, null, array('class'=>'form-control','required'=>'true'))}}<br>
 		{{ Form::label('torneo_id','Torneo')}}
 		{{ Form::select('torneo_id', $torneos, null, array('class'=>'form-control','required'=>'true'))}}<br>
 		{{ Form::label('fecha')}}
@@ -11,8 +11,8 @@
 <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 	<div class="form-group">
 		{{ Form::label('goles_f','Goles a Favor')}}
-		{{ Form::text('goles_f', null, array('class'=>'form-control','required'=>'true'))}}<br>
+		{{ Form::number('goles_f', null, array('class'=>'form-control','required'=>'true'))}}<br>
 		{{ Form::label('goles_c','Goles en contra')}}
-		{{ Form::text('goles_c', null, array('class'=>'form-control','required'=>'true'))}}<br>
+		{{ Form::number('goles_c', null, array('class'=>'form-control','required'=>'true'))}}<br>
 	</div>
 </div>
